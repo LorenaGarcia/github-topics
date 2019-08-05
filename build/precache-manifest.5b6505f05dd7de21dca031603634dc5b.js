b9287ab752b38f02dc41b119bcfc30ce6470045a
Lorena Garcia <lorena.garcia@element.com.mx>
2019-08-05 08:37:49 -0500

    save

---
+++ build/precache-manifest.5b6505f05dd7de21dca031603634dc5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3615968e51b91c86881af68c9ddc03a2",
+    "url": "/github-topics/index.html"
+  },
+  {
+    "revision": "ab5412aef59a1930e150",
+    "url": "/github-topics/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "873e0fdc2a23eab54ffe",
+    "url": "/github-topics/static/js/2.7262e8af.chunk.js"
+  },
+  {
+    "revision": "ab5412aef59a1930e150",
+    "url": "/github-topics/static/js/main.6c74efca.chunk.js"
+  },
+  {
+    "revision": "3797f60f7559d5691d11",
+    "url": "/github-topics/static/js/runtime~main.e36132b8.js"
+  }
+]);
